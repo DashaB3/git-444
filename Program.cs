439e2f2de06b4b72100090d776723df9c8a3cf37
DashaB3 <stud0000248072@study.utmn.ru>
2021-11-26 19:44:20 +0500

    add 1 header

--- Program.cs
+++ Program.cs
@@ -6,9 +6,27 @@ namespace ConsoleApp1_Git_1
     {
         static void Main(string[] args)
         {
-                    
-            string s = "mqkamgk eee PASSWORD  lmgll jjgdnb PASSWORD";
-            Console.WriteLine(s);
+          Random rand = new Random();
+            var ch = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            string s = "mqkamgk eee PASSWORD  lmgll";
+            string[] mas = s.Split(' ');
+            int len = mas.Length;
+            for(int i=0;i<len;i++)
+            {
+
+                if(mas[i]== "PASSWORD")
+                {
+                    var f = ch[rand.Next(ch.Length)].ToString();
+                    mas[i] = f;
+                    Console.WriteLine(mas[i]);
+                }
+
+
+            }
+            foreach(var n in mas)
+            {
+                Console.Write(n+" ");
+            }
             
         }
     }
