96ead7c7ca146e29b03234248c3db02eba1dd46b
DashaB3 <stud0000248072@study.utmn.ru>
2021-11-26 19:35:42 +0500

    init git

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1_Git_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                    
+            string s = "mqkamgk eee PASSWORD  lmgll jjgdnb PASSWORD";
+            Console.WriteLine(s);
+            
+        }
+    }
+}
